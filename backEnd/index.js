6442740f6b3ed6a2a7847a9fc95e980e90bf5dc0
ellaelisimo <gabriela.barsukova@gmail.com>
2022-12-29 10:18:22 +0200

    commit

--- backEnd/index.js
+++ backEnd/index.js
@@ -15,29 +15,54 @@ const DBCollectionUsers = process.env.DBCollectionUsers;
 app.use(express.json());
 app.use(cors());
 
-app.get("/users", async (req, res) => {
-  const connection = await client.connect();
-  const data = await connection
-    .db(DB)
-    .collection(DBCollectionUsers)
-    .find()
-    .toArray();
-  await connection.close();
-  return res.send(data);
-});
+// app.get("/users", async (req, res) => {
+//   const connection = await client.connect();
+//   const data = await connection
+//     .db(DB)
+//     .collection(DBCollectionUsers)
+//     .find()
+//     .toArray();
+//   await connection.close();
+//   return res.send(data);
+// });
 
-app.get("/services", async (req, res) => {
+app.get("/memberships", async (req, res) => {
   try {
     const connection = await client.connect();
-    const data = await connection
+    const memberships = await connection
       .db(DB)
       .collection(DBCollectionServices)
       .find()
+      .sort({ price: 1 })
       .toArray();
     await connection.close();
-    return res.send(data);
+    return res.send(memberships);
   } catch (err) {
     res.status(500).send({ err });
+  }
+});
+
+app.delete("/membership/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(400).send("ID was not provided.").end();
+    return;
+  }
+
+  try {
+    const con = await client.connect();
+    const deleteMembership = await con
+      .db(DB)
+      .collection(DBCollectionServices)
+      .deleteOne({ _id: ObjectId(id) });
+
+    await con.close();
+
+    res.send(deleteMembership).end();
+  } catch (err) {
+    res.status(500).send({ err }).end();
+    throw Error(err);
   }
 });
 
--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,11 +1,70 @@
 const getMemberships = async () => {
   try {
-    const response = await fetch("http://localhost:5000/services/");
+    const response = await fetch("http://localhost:5000/memberships/");
     const memberships = await response.json();
-    console.log(memberships);
-    return memberships;
+
+    renderMembershipTable(memberships);
   } catch (error) {
     console.error(error);
   }
 };
 getMemberships();
+
+const currency = "$";
+
+const renderMembershipTable = (memberships) => {
+  const sectionContainer = document.querySelector("#cards-section");
+
+  sectionContainer.replaceChildren();
+
+  memberships.forEach((membership) => {
+    const { _id, name, price, description } = membership;
+
+    const membershipCard = document.createElement("div");
+    membershipCard.className = "membership-card";
+
+    const membershipTypeCard = document.createElement("div");
+    membershipTypeCard.className = "membership-card-type";
+
+    const membershipType = document.createElement("h2");
+    const membershipDescriptionEl = document.createElement("p");
+
+    const deleteMembershipContainer = document.createElement("div");
+    deleteMembershipContainer.className = "delete-container-membership";
+
+    membershipType.textContent = `${currency}${price} ${name}`;
+    membershipDescriptionEl.textContent = description;
+
+    const deleteMembershipButton = document.createElement("button");
+    deleteMembershipButton.id = _id;
+    deleteMembershipButton.className = "delete-membership-button";
+    deleteMembershipButton.innerHTML = '<i class="fa fa-trash"> </i>';
+
+    membershipTypeCard.append(membershipType, membershipDescriptionEl);
+
+    deleteMembershipContainer.append(deleteMembershipButton);
+
+    membershipCard.append(membershipTypeCard, deleteMembershipContainer);
+
+    sectionContainer.append(membershipCard);
+
+    const deleteMembership = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/membership/${deleteMembershipButton.id}`,
+          {
+            method: "DELETE",
+          }
+        );
+        const isPostDeleted = response.ok;
+
+        if (isPostDeleted) {
+          await getMemberships();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    deleteMembershipButton.addEventListener("click", deleteMembership);
+  });
+};
